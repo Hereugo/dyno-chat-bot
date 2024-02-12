d04bfbbbd229b117a30e12e6f7dfb230a604c566
Амир Нурмухамбетов <thereug6@gmail.com>
2024-02-12 20:21:46 +0100

    probably finished bot

---
+++ README.md
@@ -0,0 +1,17 @@
+# Dyno Chat Bot 
+
+## Description
+
+A simple telegram bot that talks in Dyno Channel
+
+## TODO
+
+- [x] Connect to Channel
+- [x] Get latest channel message
+- [-] Integrate chatGPT API
+- [x] command /dyno <question>. Answers the question.
+- [ ] Hosting? Raspberry PI будет на полу в комнате работать
+
+
+В канале пост для текстовых сооб 
+(если хотите там пишите >.<)
---
+++ index.py
@@ -0,0 +1,85 @@
+import os
+import logging
+
+from telegram import ForceReply, Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
+from dotenv import load_dotenv
+
+from wonderwords import RandomSentence
+
+load_dotenv()
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
+
+TOKEN: str = os.getenv("TOKEN")
+
+s: RandomSentence = RandomSentence()
+
+
+def generate_response(text: str) -> str:
+    return s.sentence()
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}!",
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+async def dyno(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # TODO: Get text after command
+    await update.message.reply_text(
+        generate_response(""),
+    )
+
+
+async def channel_message_reply(
+    update: Update, context: ContextTypes().DEFAULT_TYPE
+) -> None:
+    """Reply to a chnanel message."""
+    try:
+        if update.message.forward_origin.chat.type == "channel":
+            text: str = update.message.text
+            response: str = generate_response(text)
+
+            await update.message.reply_text(response)
+    except Exception as e:
+        logger.error(e)
+        pass
+
+
+def main() -> None:
+    """Start the bot."""
+
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(TOKEN).build()
+
+    application.add_handler(CommandHandler("start", start))
+
+    application.add_handler(CommandHandler("dyno", dyno))
+
+    # on non command i.e message - echo the message on Telegram
+    application.add_handler(
+        MessageHandler(filters.TEXT & ~filters.COMMAND, channel_message_reply)
+    )
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
